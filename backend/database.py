07cb4155f44d25bf79dca3235e45eb0814fbf0ea
satyavardhan <satyavardhand19@gmail.com>
2026-03-23 06:24:34 +0000

    switch to sqlite

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import SQLModel, create_engine, Session
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "DATABASE_URL = "sqlite:///./test.db"")
+DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(
     DATABASE_URL,
@@ -9,9 +9,11 @@ engine = create_engine(
     connect_args={"check_same_thread": False}  # ✅ IMPORTANT
 )
 
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
 def get_session():
     with Session(engine) as session:
         yield session
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+
